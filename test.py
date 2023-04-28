0d0e2f5f075e18ee621b2f0984874d3f9dcc6d7c
suraj <surajee456@gmail.com>
2023-04-28 23:45:00 +0900

    python file  cast

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 print("Hello World!")
-name = input("enter your name?")
-print(f"hello {name}")
+# name = input("enter your name?")
+# print(f"hello {name}")
+
+number = int(input("number?"))
+
+if number > 0:
+    print(f"{number} is greater than zero")
+elif number < 0:
+    print(f"{number} is negative")
+else:
+    print(f"{number} is zero ")

9ee1a9fb7be9814da3e860045c81b7e8a6a78dac
suraj <surajee456@gmail.com>
2023-04-29 02:42:45 +0900

    python file

--- test.py
+++ test.py
@@ -2,6 +2,36 @@ print("Hello World!")
 # name = input("enter your name?")
 # print(f"hello {name}")
 
+name = "hello"
+
+print(name[0])
+
+s = set()
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+print(f"set before remove{s}")
+print(f"s length is {len(s)}")
+
+s.remove(4)
+print(f" set after remove {s}")
+print(f"s length is {len(s)}")
+
+# for
+friends = ["tom", "jack", "harry"]
+test = len(friends)
+for i in range(test):
+    print(friends[i])
+
+# for
+for i in range(6):
+    print(i)
+
+# for
+for i in range(1, 10):
+    print(i)
+
 number = int(input("number?"))
 
 if number > 0:
